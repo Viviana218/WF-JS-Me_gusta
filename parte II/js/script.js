f6a2d9d256211642eb7695d38fc237256aa37b4a
Viviana Medina <vmedinaarango@gmail.com>
2022-08-22 17:50:37 -0500

    nuevo

---
+++ parte II/js/script.js
@@ -0,0 +1,20 @@
+var n1 = 0
+function incrementa1 () {
+    n1++;
+    var conteo=document.querySelector ("#numero1");
+    conteo.innerText=n1
+}
+
+var n2 = 0
+function incrementa2 () {
+    n2++;
+    var conteo=document.querySelector ("#numero2");
+    conteo.innerText=n2
+}
+
+var n3 = 0
+function incrementa3 () {
+    n3++;
+    var conteo=document.querySelector ("#numero3");
+    conteo.innerText=n3
+}
